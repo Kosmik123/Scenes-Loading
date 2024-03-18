372b03ae8fdc0c2f0d42d1849c9d884beabced6e
Michal <malpa.w.klapkach@gmail.com>
2024-03-18 16:43:18 +0100

    Change progress bar controllers to inherit from MonoBehaviour

---
+++ Scripts/Runtime/UI/LoadingProgressBarImage.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bipolar.SceneManagement.UI
+{
+    public class LoadingProgressBarImage : MonoBehaviour
+    {
+        [SerializeField]
+        private Image progressBar;
+
+        private void OnEnable()
+        {
+            progressBar.fillAmount = LoadingManager.Instance.Progress;
+            LoadingManager.OnLoadingProgressChanged += OnLoadingProgressChanged;
+        }
+
+        private void OnLoadingProgressChanged(float progress)
+        {
+            progressBar.fillAmount = progress;
+        }
+
+        private void OnDisable()
+        {
+            LoadingManager.OnLoadingProgressChanged -= OnLoadingProgressChanged;
+        }
+    }
+}
---
+++ Scripts/Runtime/UI/LoadingProgressBarSlider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bipolar.SceneManagement.UI
+{
+    public class LoadingProgressBarSlider : MonoBehaviour
+    {
+        [SerializeField]
+        private Slider progressBar;
+
+    }
+}
