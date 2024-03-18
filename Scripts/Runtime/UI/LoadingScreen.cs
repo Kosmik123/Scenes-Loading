f61243a37becc51c954f7cf8ddbccfa7390916a1
Michal <malpa.w.klapkach@gmail.com>
2024-03-18 16:41:57 +0100

    Make LoadingScreen class handle only showing and hiding the screen (no inheritance)

--- Scripts/Runtime/UI/LoadingScreen.cs
+++ Scripts/Runtime/UI/LoadingScreen.cs
@@ -4,22 +4,29 @@ namespace Bipolar.SceneManagement.UI
 {
     public class LoadingScreen : MonoBehaviour
     {
-        protected virtual void OnLoadingStarted(ScenesContext context) { }
-        protected virtual void OnLoadingProgressChanged(float progress) { }
-        protected virtual void OnLoadingEnded(ScenesContext context) { }
-
         private void Awake()
         {
-            LoadingManager.Instance.OnLoadingStarted += OnLoadingStarted;
-            LoadingManager.Instance.OnLoadingProgressChanged += OnLoadingProgressChanged;
-            LoadingManager.Instance.OnLoadingEnded += OnLoadingEnded;
+            var methodToCall = LoadingManager.Instance.IsLoading ? (System.Action)OnLoadingStarted : OnLoadingEnded;
+            methodToCall.Invoke();
+
+            LoadingManager.OnLoadingStarted += OnLoadingStarted;
+            LoadingManager.OnLoadingEnded += OnLoadingEnded;
+        }
+
+        private void OnLoadingStarted() 
+        {
+            gameObject.SetActive(true);    
+        }
+
+        private void OnLoadingEnded() 
+        {
+            gameObject.SetActive(false);    
         }
 
         private void OnDestroy()
         {
-            LoadingManager.Instance.OnLoadingStarted -= OnLoadingStarted;
-            LoadingManager.Instance.OnLoadingProgressChanged -= OnLoadingProgressChanged;
-            LoadingManager.Instance.OnLoadingEnded -= OnLoadingEnded;
+            LoadingManager.OnLoadingStarted -= OnLoadingStarted;
+            LoadingManager.OnLoadingEnded -= OnLoadingEnded;
         }
     }
 }
