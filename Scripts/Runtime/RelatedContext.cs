fd69e88c7b795e3e4c50d925124e6ea5650dda39
Michal <malpa.w.klapkach@gmail.com>
2024-03-18 16:44:42 +0100

    Create RelatedContext (which binds scene to a specific context)

---
+++ Scripts/Runtime/RelatedContext.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace Bipolar.SceneManagement
+{
+    public class RelatedContext : MonoBehaviour
+    {
+        [SerializeField]
+        private ScenesContext context;
+
+        private void Awake()
+        {
+            if (LoadingManager.Instance == null)
+            {
+                LoadingManager.OnInstanceCreated += LoadContext;
+            }
+            else if (LoadingManager.Instance.IsLoading)
+            {
+                LoadingManager.OnLoadingEnded += LoadContext;
+            }
+            else
+            {
+                LoadContext();
+            }
+        }
+
+
+        private void LoadContext()
+        {
+            LoadingManager.OnLoadingEnded -= LoadContext;
+            LoadingManager.OnInstanceCreated -= LoadContext;
+
+            if (LoadingManager.Instance.CurrentContext != context)
+            {
+                LoadingManager.LoadContext(context);
+            }
+        }
+    } 
+}
